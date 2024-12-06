827f07a3604baaab07ace85cf10740f0b0b2c33e
Your Name <you@example.com>
2024-12-06 10:45:47 +0300

    Initial commit

---
+++ newapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from newapp.apps import NewappConfig
+from newapp.views import home
+
+
+app_name = NewappConfig.name
+
+
+urlpatterns = [
+    path('', home, name='index')
+]
