7fa950d24861f06cec768bbd8f3274fa12b50577
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2025-02-26 09:27:52 +0300

    small fixes

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('users/', include('users.urls', namespace='users')),
+    path('', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
